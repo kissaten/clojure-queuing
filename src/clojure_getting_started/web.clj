023a2315b61bccaaa83a1acb1cb4b571bfbe18c2
Joe Kutner <jpkutner@gmail.com>
2014-12-20 17:23:13 -0600

    fixed request id logging

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -11,13 +11,15 @@
    :headers {"Content-Type" "text/plain"}
    :body (pr-str ["Hello" :from 'Heroku])})
 
+(defn request-id-filter [request])
+
 (defroutes app
+  (ANY "*" {:keys [headers params body] :as request}
+    (println (format "request_id=%s" (get headers "x-request-id"))))
   (GET "/" []
-       (splash))
-  (GET "/request_id" {:keys [headers params body] :as request} 
-       (format "<h1>%s</h1>" headers))
+    (splash))
   (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
+    (route/not-found (slurp (io/resource "404.html")))))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
