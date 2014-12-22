4a10fa9f36d430f12938933edda9a3ec01562b05
Joe Kutner <jpkutner@gmail.com>
2014-12-21 19:11:06 -0600

    added queuing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# clojure-getting-started
+# Clojure Queuing Example
 
 A barebones Clojure app, which can easily be deployed to Heroku.  
 
@@ -9,12 +9,16 @@ This application support the [Getting Started with Clojure](https://devcenter.he
 
 Make sure you have Clojure installed.  Also, install the [Heroku Toolbelt](https://toolbelt.heroku.com/).
 
+Then [install RabbitMQ](http://www.rabbitmq.com/download.html) and make sure it's running.
+
+Now download and start the app:
+
 ```sh
-$ git clone https://github.com/heroku/clojure-getting-started.git
-$ cd clojure-getting-started
+$ git clone https://github.com/heroku/clojure-queuing.git
+$ cd clojure-queuing
 $ lein repl
-user=> (require 'clojure-getting-started.web)
-user=>(def server (clojure-getting-started.web/-main))
+user=> (require 'clojure-queuing.web)
+user=>(def server (clojure-queuing.web/-main))
 ```
 
 Your app should now be running on [localhost:5000](http://localhost:5000/).
@@ -23,6 +27,7 @@ Your app should now be running on [localhost:5000](http://localhost:5000/).
 
 ```sh
 $ heroku create
+$ heroku addons:add cloudamqp
 $ git push heroku master
 $ heroku open
 ```
@@ -32,4 +37,3 @@ $ heroku open
 For more information about using Clojure on Heroku, see these Dev Center articles:
 
 - [Clojure on Heroku](https://devcenter.heroku.com/categories/clojure)
-
---
+++ src/queuing/web.clj
@@ -0,0 +1,53 @@
+(ns queuing.web
+  (:gen-class)
+  (:require
+    [langohr.core      :as rmq]
+    [langohr.channel   :as lch]
+    [langohr.queue     :as lq]
+    [langohr.consumers :as lc]
+    [langohr.basic     :as lb]
+    [compojure.core    :refer [defroutes GET]]
+    [compojure.handler :refer [site]]
+    [compojure.route   :as route]
+    [ring.adapter.jetty :as jetty]
+    [environ.core      :refer [env]]))
+
+(def ^{:const true} default-exchange-name "")
+
+(def qname "langohr.examples.hello-world")
+
+(defn message-handler
+  [ch {:keys [content-type delivery-tag] :as meta} ^bytes payload]
+  (println
+    (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s"
+    (String. payload "UTF-8") delivery-tag content-type)))
+
+(def amqp-url (get (System/getenv) "CLOUDAMQP_URL" "amqp://guest:guest@localhost:5672"))
+
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Published message!"})
+
+(defroutes app
+  (GET "*" []
+    (let [conn  (rmq/connect {:uri amqp-url})
+          ch    (lch/open conn)]
+    (lb/publish ch default-exchange-name qname "Hello!" {:content-type "text/plain" :type "greetings.hi"})
+    (rmq/close ch)
+    (rmq/close conn)
+    (splash))))
+
+(defn -main [& [port]]
+  (let [conn  (rmq/connect {:uri amqp-url})
+        ch    (lch/open conn)
+        port  (Integer. (or port (env :port) 5000))]
+    (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
+    (lq/declare ch qname {:exclusive false :auto-delete true})
+    (lc/subscribe ch qname message-handler {:auto-ack true})
+    (jetty/run-jetty (site #'app) {:port port :join? false})
+    (.addShutdownHook (Runtime/getRuntime) (Thread. #(do (rmq/close ch) (rmq/close conn))))))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
